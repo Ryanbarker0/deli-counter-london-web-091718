ffec459246b658ddb675202511258216d6887c5c
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-11 12:43:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,11 @@ katz_deli = []
 def line(katz_deli)
   i = 0
   if katz_deli.length > 0
-    @deli_line = katz_deli.each do |line_number|
-      puts "#{i += 1}. #{line_number}"
+      deli_line = []
+      katz_deli.each_with_index do |name, num|
+      deli_line << " #{num += 1}. #{name}"
     end
-    "The line is currently: #{@deli_line}"
+    "The line is currently:#{@deli_line}"
   else
     puts "The line is currently empty."
   end
