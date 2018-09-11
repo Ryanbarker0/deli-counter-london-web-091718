d21d3f1dfe590765cd4c654aab32d350e7a165b2
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-11 12:53:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,12 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length == 0
+    katz_deli.push(name)
+  else
+    katz_deli.unshift(name)
+  end
+  katz_deli
+end
