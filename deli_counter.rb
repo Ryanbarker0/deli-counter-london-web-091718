0f8ceee42f50a61c5bb8244df4f1ada5fd9ea2d8
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-11 12:48:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,9 @@ def line(katz_deli)
   if katz_deli.length > 0
       deli_line = Array.new
       katz_deli.each_with_index do |name, i|
-        deli_line << " #{i + 1}. #{name}"
-        puts "The line is currently:#{@deli_line.join}"
+        deli_line << " #{i + 1}. #{name}"  
     end
-
+    puts "The line is currently:#{deli_line.join}"
   else
     puts "The line is currently empty."
   end
