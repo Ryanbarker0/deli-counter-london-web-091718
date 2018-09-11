b6d02bf30f54e235055c8722d4e597130b7b5fa6
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-11 12:19:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,9 @@
 katz_deli = []
 
 def line(katz_deli)
+  i = 1
   if katz_deli.length > 0
-    puts katz_deli
+    puts "The line is currently: #{katz_deli}"
   else
     puts "The line is currently empty."
   end
