433079d72f3b8ba99da74c7cf79814041b7e5409
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-11 12:46:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length > 0
       deli_line = Array.new
-      katz_deli.each_with_index do |name, num|
-        deli_line << " #{num += 1}. #{name}"
+      katz_deli.each_with_index do |name, i|
+        deli_line << " #{i + 1}. #{name}"
     end
     "The line is currently:#{@deli_line.join}"
   else
